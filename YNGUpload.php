9090e1c46c71a5d8d3120e1028a12347572d8d60
dav9854 <amador2122@gmail.com>
2017-07-17 09:26:28 -0500

    modified customJS for printer DD in file upload of UI

--- YNGUpload.php
+++ YNGUpload.php
@@ -541,16 +541,84 @@ $selectprintQuery = mysqli_query($dbConnection, $selectMatType);
 
 
     <script>
-
-
-        var dd_AP = JSON.stringify(document.getElementById("currentPrinter").innerText);
-
         var client1 = new OctoPrintClient({baseurl: printerURL, apikey: apiKey});
+
         // easy ele call
         function $id(id){
             return document.getElementById(id);
         }
 
+
+
+
+
+        var dd_AP = JSON.stringify(document.getElementById("currentPrinter").innerText);
+        console.log('active Printer:'+dd_AP);
+
+        function ddPrinter(){
+
+
+            if (dd_AP = '"reveal3D"'){
+                console.log("dd_AP: "+dd_AP);
+
+                printers = {"reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
+
+                nozzle = {"reveal3D": [" Single", " Duplication"]};
+                
+            }else if(dd_AP = '"FRANK3"'){
+                console.log("dd_AP: "+dd_AP);
+
+                printers = {"FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
+
+                nozzle = {"FRANK3": [" Single", " Duplication"]};
+                
+            }else if(dd_AP = '"GT"'){
+                console.log("dd_AP: "+dd_AP);
+
+                printers = {"GT": [" 1", " 2", " 3", " 4"]};
+
+                nozzle = {"GT": [" Single", " Duplication"]};
+            };
+
+            console.log('AFTER ALL: '+dd_AP);
+
+
+
+
+            // printers = {
+            //     "reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
+            //     "FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
+            //     "GT": [" 1", " 2", " 3", " 4"]
+            // };
+
+            // nozzle = {
+            //     "reveal3D": [" Single", " Duplication"],
+            //     "FRANK3": [" Single", " Duplication"],
+            //     "GT": [" Single", " Duplication"]
+            // };
+
+
+            $.each(printers, function(key, value) {
+
+                $('#printerSelection').append($('<option />').text(key));
+            });
+
+
+            // Populates Availble options for select based off Selected Printer
+            $('#printerSelection').change(function() {
+                var key = $(this).val();
+                $('#zoneSelection').empty();
+                $('#nozzleSelection').empty();
+                for (var i in printers[key]) {
+                    $('#zoneSelection').append('<option>' + printers[key][i] + '</option>');
+                }
+                for (var i in nozzle[key]) {
+                    $('#nozzleSelection').append('<option>' + nozzle[key][i] + '</option>');
+                }
+            }).trigger('change');
+        }
+
+        ddPrinter();
 
         function getPrinterState(){
 
--- js/CustomJS.js
+++ js/CustomJS.js
@@ -1,63 +1,67 @@
-//jQuery
+// //jQuery
 
 
-// #### Runs Dynamic  Fill for  Selection in YNGUpload.php ####
-// var printers = "";
-// var nozzle = "";
+// // #### Runs Dynamic  Fill for  Selection in YNGUpload.php ####
+// // var printers = "";
+// // var nozzle = "";
 
 
-if (dd_AP = '"reveal3D"'){
-    console.log("dd_AP: "+dd_AP);
+// if (dd_AP = '"reveal3D"'){
+//     console.log("dd_AP: "+dd_AP);
 
-    printers = {"reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
+//     printers = {"reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
 
-    nozzle = {"reveal3D": [" Single", " Duplication"]};
-}
-if(dd_AP = '"FRANK3"'){
-    console.log("dd_AP: "+dd_AP);
+//     nozzle = {"reveal3D": [" Single", " Duplication"]};
+    
+// }else if(dd_AP = '"FRANK3"'){
+//     console.log("dd_AP: "+dd_AP);
 
-    printers = {"FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
+//     printers = {"FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"]};
 
-    nozzle = {"FRANK3": [" Single", " Duplication"]};
-}
-if(dd_AP = '"GT"'){
-    console.log("dd_AP: "+dd_AP);
+//     nozzle = {"FRANK3": [" Single", " Duplication"]};
+    
+// }else if(dd_AP = '"GT"'){
+//     console.log("dd_AP: "+dd_AP);
 
-    printers = {"GT": [" 1", " 2", " 3", " 4"]};
+//     printers = {"GT": [" 1", " 2", " 3", " 4"]};
 
-    nozzle = {"GT": [" Single", " Duplication"]};
-};
-
-
-// printers = {
-//     "reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
-//     "FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
-//     "GT": [" 1", " 2", " 3", " 4"]
+//     nozzle = {"GT": [" Single", " Duplication"]};
 // };
 
-// nozzle = {
-//     "reveal3D": [" Single", " Duplication"],
-//     "FRANK3": [" Single", " Duplication"],
-//     "GT": [" Single", " Duplication"]
-// };
+// console.log('AFTER ALL: '+dd_AP);
 
 
-$.each(printers, function(key, value) {
-
-    $('#printerSelection').append($('<option />').text(key));
-});
 
 
-// Populates Availble options for select based off Selected Printer
-$('#printerSelection').change(function() {
-    var key = $(this).val();
-    $('#zoneSelection').empty();
-    $('#nozzleSelection').empty();
-    for (var i in printers[key]) {
-        $('#zoneSelection').append('<option>' + printers[key][i] + '</option>');
-    }
-    for (var i in nozzle[key]) {
-        $('#nozzleSelection').append('<option>' + nozzle[key][i] + '</option>');
-    }
-}).trigger('change');
+// // printers = {
+// //     "reveal3D": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
+// //     "FRANK3": [" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", " 10", " 11", " 12", " 13", " 14", " 15", " 16"],
+// //     "GT": [" 1", " 2", " 3", " 4"]
+// // };
+
+// // nozzle = {
+// //     "reveal3D": [" Single", " Duplication"],
+// //     "FRANK3": [" Single", " Duplication"],
+// //     "GT": [" Single", " Duplication"]
+// // };
+
+
+// $.each(printers, function(key, value) {
+
+//     $('#printerSelection').append($('<option />').text(key));
+// });
+
+
+// // Populates Availble options for select based off Selected Printer
+// $('#printerSelection').change(function() {
+//     var key = $(this).val();
+//     $('#zoneSelection').empty();
+//     $('#nozzleSelection').empty();
+//     for (var i in printers[key]) {
+//         $('#zoneSelection').append('<option>' + printers[key][i] + '</option>');
+//     }
+//     for (var i in nozzle[key]) {
+//         $('#nozzleSelection').append('<option>' + nozzle[key][i] + '</option>');
+//     }
+// }).trigger('change');
 
