899148e3d5191073f7da29884f796d60985e259b
dav9854 <amador2122@gmail.com>
2017-07-18 14:51:45 -0500

    added the .yngupdate.sh 2

--- .yngupdate.sh
+++ .yngupdate.sh
@@ -4,4 +4,4 @@ chown -R pi:www-data /var/www/html/Manipulate/.git
 chown -R pi:www-data /var/www/html/Reveal3D-UI/.git
 chmod -R 775 /var/www/html/Manipulate/.git
 chmod -R 775 /var/www/html/Reveal3D-UI/.git
-echo "change complete"
+echo "file changes complete"
