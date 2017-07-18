a3401238c1ab0a5fd0af187c92b05162b854bc66
dav9854 <amador2122@gmail.com>
2017-07-18 14:20:54 -0500

    added the .yngupdate.sh

--- .yngupdate.sh
+++ .yngupdate.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-chown -R odroid:www-data /var/www/html/Manipulate/.git
-chown -R odroid:www-data /var/www/html/Reveal3D-UI/.git
+chown -R pi:www-data /var/www/html/Manipulate/.git
+chown -R pi:www-data /var/www/html/Reveal3D-UI/.git
 chmod -R 775 /var/www/html/Manipulate/.git
 chmod -R 775 /var/www/html/Reveal3D-UI/.git
+echo "change complete"
