701c842d082194b447728425d61ea34d9932e3a5
dav9854 <amador2122@gmail.com>
2017-07-19 13:22:21 -0500

    permission fix, dir search

--- .yngupdate.sh
+++ .yngupdate.sh
@@ -1,7 +1,22 @@
 #/bin/bash
 
-chown -R pi:www-data /var/www/html/Manipulate/.git
-chown -R pi:www-data /var/www/html/Reveal3D-UI/.git
-chmod -R 775 /var/www/html/Manipulate/.git
-chmod -R 775 /var/www/html/Reveal3D-UI/.git
-echo "file changes complete"
+if [ -d "home/pi" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  chown -R pi:www-data /var/www/html/Manipulate/.git
+  chown -R pi:www-data /var/www/html/Reveal3D-UI/.git
+  chmod -R 775 /var/www/html/Manipulate/.git
+  chmod -R 775 /var/www/html/Reveal3D-UI/.git
+  echo "file changes complete"
+fi
+
+if [ -d "home/odroid/" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  chown -R odroid:www-data /var/www/html/Manipulate/.git
+  chown -R odroid:www-data /var/www/html/Reveal3D-UI/.git
+  chmod -R 775 /var/www/html/Manipulate/.git
+  chmod -R 775 /var/www/html/Reveal3D-UI/.git
+  echo "file changes complete"
+fi
+
+
+
