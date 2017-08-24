041b5076e0a7f52287afaaf00e5d46fd72293237
dav9854 <amador2122@gmail.com>
2017-08-24 16:57:55 -0500

    updated headers for runStatus deletion

--- .fixTable.php
+++ .fixTable.php
@@ -36,7 +36,7 @@
 			}
 
 			//### Import Table ###
-			if($checkTableDelete == 0){
+			if($checkTableDelete == 1){
 		        echo "Importing Table...\n";
 	        	$sql2 = "CREATE TABLE IF NOT EXISTS `yngPrints` (`task_id` int(23) NOT NULL AUTO_INCREMENT,`file` varchar(250) COLLATE utf8_unicode_ci NOT NULL, `file_location` varchar(250) COLLATE utf8_unicode_ci NOT NULL, `zone` int(11) NOT NULL, `statusValue` text COLLATE utf8_unicode_ci NOT NULL,  `bigmaxX` text COLLATE utf8_unicode_ci NOT NULL, `smallminX` text COLLATE utf8_unicode_ci NOT NULL, `bigmaxY` text COLLATE utf8_unicode_ci NOT NULL, `smallminY` text COLLATE utf8_unicode_ci NOT NULL, `bigmaxZ` text COLLATE utf8_unicode_ci NOT NULL,`progCheck` int(11) NOT NULL, `errorLog` text COLLATE utf8_unicode_ci NOT NULL, `printerType` varchar(15) COLLATE utf8_unicode_ci NOT NULL, `materialType` varchar(15) COLLATE utf8_unicode_ci NOT NULL, `nozzleMode` text COLLATE utf8_unicode_ci NOT NULL, PRIMARY KEY (`task_id`), UNIQUE KEY `task_id` (`task_id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=369" ;
 		        	echo $sql2;
