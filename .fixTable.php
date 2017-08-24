1dbb591e71b4cdd42d913b715038c925e627f0a1
dav9854 <amador2122@gmail.com>
2017-08-24 16:59:21 -0500

    updated headers for runStatus deletion

--- .fixTable.php
+++ .fixTable.php
@@ -29,14 +29,13 @@
 	         	if(! $retval){
 	            	die('Could not delete table: ' . mysql_error());
 	         	}
-				$checkTableDelete = 1;
 	         	echo "Table deleted successfully\n";
 			}else{
 				echo "Cannot Delete what does not exist<br />";
 			}
 
 			//### Import Table ###
-			if($checkTableDelete == 1){
+			if($checkTableDelete == 0){
 		        echo "Importing Table...\n";
 	        	$sql2 = "CREATE TABLE IF NOT EXISTS `yngPrints` (`task_id` int(23) NOT NULL AUTO_INCREMENT,`file` varchar(250) COLLATE utf8_unicode_ci NOT NULL, `file_location` varchar(250) COLLATE utf8_unicode_ci NOT NULL, `zone` int(11) NOT NULL, `statusValue` text COLLATE utf8_unicode_ci NOT NULL,  `bigmaxX` text COLLATE utf8_unicode_ci NOT NULL, `smallminX` text COLLATE utf8_unicode_ci NOT NULL, `bigmaxY` text COLLATE utf8_unicode_ci NOT NULL, `smallminY` text COLLATE utf8_unicode_ci NOT NULL, `bigmaxZ` text COLLATE utf8_unicode_ci NOT NULL,`progCheck` int(11) NOT NULL, `errorLog` text COLLATE utf8_unicode_ci NOT NULL, `printerType` varchar(15) COLLATE utf8_unicode_ci NOT NULL, `materialType` varchar(15) COLLATE utf8_unicode_ci NOT NULL, `nozzleMode` text COLLATE utf8_unicode_ci NOT NULL, PRIMARY KEY (`task_id`), UNIQUE KEY `task_id` (`task_id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=369" ;
 		        	echo $sql2;
